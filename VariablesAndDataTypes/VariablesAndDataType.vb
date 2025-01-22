a66033744e62dd852f5329a159b86832d63ed3e6
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-01-22 08:52:01 -0700

    Added 3 variables  with 2 different data types and some small operations using them.

--- VariablesAndDataTypes/VariablesAndDataType.vb
+++ VariablesAndDataTypes/VariablesAndDataType.vb
@@ -1,6 +1,30 @@
-﻿Module VariablesAndDataType
+﻿''Angel Nava
+'Spring 2025
+'RCET2265
+'VariablesAndDataType
+'link needed
+Option Explicit On
+Module VariablesAndDataType
 
     Sub Main()
+
+        Dim jellyBeans As Integer
+        Dim laffyTaffy As Integer
+        Dim baconCola As String
+
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans = 120
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans += jellyBeans
+        Console.WriteLine(jellyBeans)
+
+        laffyTaffy = 25
+        Console.WriteLine(laffyTaffy)
+
+        baconCola = "I love sugar"
+        Console.WriteLine(baconCola)
 
     End Sub
 
